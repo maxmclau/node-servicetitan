b994f5cfd7822a7ac218f95ac1e53f84682e4d06
Maxmillion McLaughlin <github@maxmclau.com>
2016-08-16 13:23:37 -0400

    Conform to promise design pattern

--- test/customers.js
+++ test/customers.js
@@ -4,17 +4,17 @@ var expect       = require("chai").expect;
 var key          = process.env.API_KEY;
 assert(key, null, 'API_KEY not set');
 
-var servicetitan = require('./../lib/index')({
+var st           = require('./../lib/index')({
   key : key
 });
 
 describe("[customers]", function() {
   it("should successfully execute GET /customers (getCustomers)", function(next) {
-    servicetitan
-      .getCustomers(function(err, res) {
-        assert.equal(err, null);
-
-        next();
-      });
+    st
+      .getCustomers()
+      .then(function(res) {
+        console.log(res);
+      next();
+    });
   });
 });
